cd4b63f4512941d63bc2ddb9b37c0560450f7368
Amar Ahmed <amar@Amars-Air.fios-router.home>
2021-10-28 02:45:19 -0400

    api calls

--- src/App.js
+++ src/App.js
@@ -3,55 +3,76 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
+  const [data, setData] = useState(null);
   useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
+    // async function getData() {
+    //   const res = await fetch('https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=1&locationId=5140&minimum=1');
+    //   const data = await res.json();
+    // }
+    // getData();
+    const locationId = locations.map(val => val.id)
+
+    const dataAll = Promise.all(locationId.map(id =>
+      fetch(`https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=1&locationId=${id}&minimum=1`).then(resp => resp.json())
+    ))
+    dataAll.then(e => setData(e))
   }, []);
+
+  function renderTable() {
+    if (data) {
+      return data.map(value => {
+        let locationName = ''
+        if (value[0]?.locationId) {
+          locationName = locationKey[value[0]?.locationId]
+        }
+         if (value[0]?.locationId) {
+          return (<tr>
+            <td>{value[0]?.locationId}</td>
+            <td>{locationName}</td>
+            <td>{value[0]?.endTimestamp}</td>
+          </tr>)        }
+
+      })
+    }
+  }
+
   return (
     <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
+      <h2>TTP-App</h2 >
+      {/* {data ? <p>{data.endTimestamp}</p> : null} */}
+      <table>
+        <tr>
+          <th>Location ID</th>
+          <th>Location Name</th>
+          <th>Time</th>
+        </tr>
+        {renderTable()}
+      </table>
+      
       <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      {/* <h2>The date according to Go is:</h2> */}
+      {/* <p>{data ? data : 'Loading date...'}</p> */}
     </main>
   );
 }
 
 export default App;
+
+const locations = [
+  { id: 8120, name: 'Washington, DC Enrollment Center' },
+  { id: 7940, name: 'Balt/Wash Intl Thurgood Marshall Airport' },
+  { id: 5140, name: 'JFK International Global Entry EC' },
+  { id: 6480, name: 'U.s. Custom House - Bowling Green' },
+  { id: 5445, name: 'Philadelphia International Airport' },
+  { id: 5400, name: 'San Juan Global Entry Enrollment Center' },
+  { id: 5142, name: 'Washington Dulles International Global Entry EC' }
+]
+const locationKey = {
+  8120: 'Washington, DC Enrollment Center',
+  7940: 'Balt/Wash Intl Thurgood Marshall Airport',
+  5140: 'JFK International Global Entry EC',
+  6480: 'U.s. Custom House - Bowling Green',
+  5445: 'Philadelphia International Airport',
+  5400: 'San Juan Global Entry Enrollment Center',
+  5142: 'Washington Dulles International Global Entry EC'
+}
